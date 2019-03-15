71074b8ae8306cd22b6881d10410e83dc211d754
MLotton <maxime.lotton@gmail.com>
2019-03-15 18:18:05 +0100

    WIP: add new scenario install-uninstall-openstack | Need to be fixed : testinfra tests

--- ansible/oVirt.v2v-conversion-host/molecule/tests-testinfra/test_uninstall.py
+++ ansible/oVirt.v2v-conversion-host/molecule/tests-testinfra/test_uninstall.py
@@ -14,79 +14,79 @@ def test_hosts_file(host):
     assert f.group == 'root'
 
 
-def test_host_infra_default_install(host):
+def test_host_infra_default_uninstall(host):
     inventory_vars = host.ansible.get_variables()
 
-    if inventory_vars.get("role_action") != "install":
-        return
+    # if inventory_vars.get("role_action") != "uninstall":
+    #     return
 
     # Check wrapper
-    assert host.package("v2v-conversion-host-wrapper").is_installed
+    assert not host.package("v2v-conversion-host-wrapper").is_installed
 
     # Check package virt-v2v
-    assert host.package("virt-v2v").is_installed
+    assert not host.package("virt-v2v").is_installed
 
     # Check packages nbdkit
-    assert host.package("nbdkit").is_installed
-    assert host.package("nbdkit-plugin-python2").is_installed
+    assert not host.package("nbdkit").is_installed
+    assert not host.package("nbdkit-plugin-python2").is_installed
 
     # Check transport vddk
     if "vddk" == inventory_vars.get("v2v_transport_method"):
         vddk_dir = host.file(inventory_vars.get("v2v_vddk_install_dir") +
                              "/vmware-vix-disklib-distrib")
-        assert (vddk_dir.exists and vddk_dir.is_directory)
-        assert host.package("nbdkit-plugin-vddk").is_installed
+        assert not (vddk_dir.exists and vddk_dir.is_directory)
+        assert not host.package("nbdkit-plugin-vddk").is_installed
 
     # Check ca-bundle
     ca_bundle = inventory_vars.get("v2v_ca_bundle")
     if ca_bundle is not None:
         ca_bundle_file = host.file("/etc/pki/ca-trust/source/anchors/"
                                    "v2v-conversion-host-ca-bundle.pem")
-        assert (ca_bundle_file.exists and
-                ca_bundle_file.user == "root" and
-                ca_bundle_file.group == "root" and
-                ca_bundle_file.mode == 0o644)
+        assert not (ca_bundle_file.exists and
+                    ca_bundle_file.user == "root" and
+                    ca_bundle_file.group == "root" and
+                    ca_bundle_file.mode == 0o644)
 
 
-def test_host_infra_openstack_install(host):
+def test_host_infra_openstack_uninstall(host):
     inventory_vars = host.ansible.get_variables()
 
-    if inventory_vars.get("role_action") != "install":
-        return
+    # if inventory_vars.get("role_action") != "uninstall":
+    #     return
 
     if inventory_vars.get("v2v_host_type") == "openstack":
 
         # Check transport ssh
         if "ssh" == inventory_vars.get("v2v_transport_method"):
             id_rsa = host.file("/root/.ssh/id_rsa")
-            assert (id_rsa.exists and id_rsa.user == "root"
-                    and id_rsa.mode == 0o600)
+            assert not (id_rsa.exists and id_rsa.user == "root"
+                        and id_rsa.mode == 0o600)
 
         # Check specific packages
-        assert host.package("python2-openstackclient").is_installed
-        assert host.package("virtio-win").is_installed
-        assert (host.package("python-six").is_installed or
-                host.package("python2-six").is_installed)
+        assert not host.package("python2-openstackclient").is_installed
+        assert not host.package("virtio-win").is_installed
+        assert not (host.package("python-six").is_installed or
+                    host.package("python2-six").is_installed)
 
         # Check cron job file (log cleanup)
         cron_script = host.file("/etc/cron.daily/virt-v2v-logs")
-        assert (cron_script.exists and cron_script.user == "root"
-                and cron_script.mode == 0o755)
+        assert not (cron_script.exists and cron_script.user == "root"
+                    and cron_script.mode == 0o755)
 
 
-def test_host_infra_rhv_install(host):
+def test_host_infra_rhv_uninstall(host):
     inventory_vars = host.ansible.get_variables()
 
-    if inventory_vars.get("role_action") != "install":
-        return
+    # if inventory_vars.get("role_action") != "uninstall":
+    #     return
 
     if inventory_vars.get("v2v_host_type") == "rhv":
 
         # Check transport ssh
         if "ssh" == inventory_vars.get("v2v_transport_method"):
             id_rsa = host.file("/var/lib/vdsm/.ssh/id_rsa")
-            assert (id_rsa.exists and id_rsa.user == "vdsm" and
-                    id_rsa.mode == 0o600)
+            assert not (id_rsa.exists and id_rsa.user == "vdsm" and
+                        id_rsa.mode == 0o600)
 
         # Check specific packages
-        assert host.package("python-ovirt-engine-sdk4").is_installed
+        assert not host.package("python-ovirt-engine-sdk4").is_installed
